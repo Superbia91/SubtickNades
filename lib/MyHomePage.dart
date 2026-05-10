58192c419d6e241ef865794237188dec75e9a9ad
Superbia Lol <shatarskii2005@gmail.com>
2026-05-10 18:47:43 +0300

    change images,add ClipRRect in card map

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -1,41 +1,90 @@
 // ignore: file_names
+import 'package:cs2/map_screen.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
-  
-final List<String> cs2Maps = const [
-    'Mirage', 'Dust II', 'Inferno', 'Nuke', 
-    'Vertigo', 'Ancient', 'Anubis'
+
+  final List<String> cs2Maps = const [
+    'Mirage',
+    'Dust II',
+    'Inferno',
+    'Nuke',
+    'Vertigo',
+    'Ancient',
+    'Anubis',
   ];
 
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.surface,
-        title: Row(mainAxisSize: MainAxisSize.min,
+        title: Row(
+          mainAxisSize: MainAxisSize.min,
           children: [
-            const Text('Subtick\nNades',style: TextStyle(fontSize: 20),textAlign: TextAlign.center),
-            Image.asset("images/logo.png",height: 70,),
-            
+            const Text(
+              'Subtick\nNades',
+              style: TextStyle(fontSize: 20),
+              textAlign: TextAlign.center,
+            ),
+            Image.asset("images/logo.png", height: 70),
           ],
         ),
         titleTextStyle: Theme.of(context).primaryTextTheme.titleLarge,
-      centerTitle: true,
+        centerTitle: true,
       ),
-      // ignore: avoid_unnecessary_containers
-      
-      body: ListView.builder(itemCount: cs2Maps.length, itemBuilder: (context, index) {
-        
-      
-      
-        return Container(height: 150,margin: EdgeInsets.all(10), child:ClipRRect(borderRadius: BorderRadius.circular(15),child:Stack( children: [SizedBox.expand(child: Image.asset("images/${cs2Maps[index].toLowerCase().replaceAll(' ', '_')}.png",fit: BoxFit.cover,),),Align(child: Text(cs2Maps[index],style: TextStyle(color: Colors.white,fontSize: 32,fontWeight: FontWeight.bold,shadows: [Shadow(color: Colors.black,blurRadius: 5,offset: Offset(2, 2))]),),)])));
-      })//TODO добавь камит о том что ты добавил скругление 
 
-        
-      );
-    
+      // ignore: avoid_unnecessary_containers
+      body: ListView.builder(
+        itemCount: cs2Maps.length,
+        itemBuilder: (context, index) {
+          return InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MapScreen(mapName: cs2Maps[index]),
+                  ),
+                );
+              },
+              child: Container(
+                height: 150,
+                margin: EdgeInsets.all(10),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Stack(
+                    children: [
+                      SizedBox.expand(
+                        child: Image.asset(
+                          "images/${cs2Maps[index].toLowerCase().replaceAll(' ', '_')}.png",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Center(
+                        child: Text(
+                          cs2Maps[index],
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 32,
+                            fontWeight: FontWeight.bold,
+                            shadows: [
+                              Shadow(
+                                color: Colors.black,
+                                blurRadius: 5,
+                                offset: Offset(2, 2),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+        },
+      ), //TODO добавь камит о том что ты добавил скругление
+    );
   }
 }
--- lib/map_screen.dart
+++ lib/map_screen.dart
@@ -1,18 +1,65 @@
 import 'package:flutter/material.dart';
 
 class MapScreen extends StatelessWidget {
-  const MapScreen({super.key,required this.mapName});
+  const MapScreen({super.key, required this.mapName});
 
   final String mapName;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        titleTextStyle: Theme.of(context).primaryTextTheme.titleLarge,
-         backgroundColor: Theme.of(context).colorScheme.surface,
-        title: Text(mapName),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          titleTextStyle: Theme.of(context).primaryTextTheme.titleLarge,
+          backgroundColor: Theme.of(context).colorScheme.surface,
+          title: Text(mapName),
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: "Smokes"),
+              Tab(text: "Flashes"),
+              Tab(text: "Molotovs"),
+            ],
+          ),
+        ),
+        body:  TabBarView(
+          children: [
+           ListView(children: [NadeCard(title: "title", description: "description", imageUrl: "images/logo.png"),NadeCard(title: "title2", description: "description2", imageUrl: "images/logo.png")],),
+           ListView(children: [Text("Flashes for $mapName", style: TextStyle(color: Colors.white),)]),
+            ListView(children: [Text("Molotovs for $mapName", style: TextStyle(color: Colors.white),)]),
+          ],
+          
+        ),
+        //
       ),
+    );
+  }
+}
+
+class NadeCard extends StatelessWidget {
+
+  final String title;
+  final String description;
+  final String imageUrl;
+  const NadeCard({super.key, required this.title, required this.description, required this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      child: Column(
+        children: [
+          ClipRRect(borderRadius: BorderRadius.vertical(top: Radius.circular(10)), child: Image.asset(  imageUrl, height: 200, width: 200, fit: BoxFit.cover,)),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(title, style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),),
+          ),
+          Text(description, style: TextStyle(fontSize: 14, color: Colors.white70),),
+        ],
+      ),
+      
+      
+
     );
   }
 }
